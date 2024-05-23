57d593720808051921c8ad41fe280d66c999fcbf
joão pedro holanda amorim <joaopedroholandaamorim@gmail.com>
2024-05-23 13:32:28 -0300

    jquery

--- jquery/1-webstore/script.js
+++ jquery/1-webstore/script.js
@@ -3,16 +3,40 @@ jQuery(function($){
     let card_link = $('.featured-item a') // class
     let home = $('#home') // id
     let p1 = $('.featured-item:nth(0)')
+    let botao = $('#form-submit')
+    let email = $('#email')
+    let modal = $('.nav-modal-open')
 
     
     // produto fora de estoque
-    p1.hide(2000,function(){
+    /*p1.hide(2000,function(){
         alert($(this).find('h4').text() +'ESGOTADO')
     });
 
     //produto de volta ao estoque
     p1.show(2000,function(){
         alert($(this).find('h4').text() +'de volta ao estoque')
+    });*/
+
+    botao.on('click', function(evento){
+        evento.preventDefault()
+        if(email.val() != '') {
+            email.animate({
+                opacity: "toggle",
+                top:"-50"
+            } , 1000,function(){
+                console.log($(this).val())
+            })
+        }
+    });
+
+    modal.on('click', function(evento){
+        evento.preventDefault()
+        let elem = $(this).attr('rel')
+        $('.modal-body').html($('#' + elem).html())
+        $('.modal-header h5.modal-title').html($(this).text())
+        let mymodal = new bootstrap.Modal($('#modalId'))
+        mymodal.show()
     });
 
     /*card-link.on('click' , function(evento){
--- jquery/introdução.md
+++ jquery/introdução.md
@@ -11,6 +11,7 @@
     - $('.class a').addclass('btn btn-dark')
     - $('.class h4').fadeIn(2000) // vai mostrar o h4
     - $('.class h4').fadeOut(2000) // vai ocultar o h4
+    - $('.class h4').toggle(2000) // vai inverter o estado do this
     - $('.class h4').css('color', '#ff0000') // altera a cor para vermelho
     - $('.class a').addclass('active')
     - $('.class a').removeclass('active')
